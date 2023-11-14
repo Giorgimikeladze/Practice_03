b2f469dd16a1e09e37419da215fd4008a3a008e2
giorgi mikeladze <mikeladzegiorgi538@outlook.com>
2023-11-15 00:31:26 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+int numb;
+Console.WriteLine("Please enter a number: ");
+string input=Console.ReadLine();
+bool is_parsed=Int32.TryParse(input, out numb);
+if (is_parsed)
+{
+    Console.WriteLine($"The square of this number is {Math.Pow(numb, 2)}");
+}
+else { 
+    Console.WriteLine("Invalid input!!!!!");
+}
